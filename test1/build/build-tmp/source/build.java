2df00789a3864308149e672657e911769cd38b39
clauviscarra <claudiavisfoss@gmail.com>
2018-02-04 17:07:58 -0500

    First commit

---
+++ test1/build/build-tmp/source/build.java
@@ -0,0 +1,30 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup() {
+	fill(0xff3f3e3f);
+	rect(10, 20, 50, 50);
+	 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ test1/build/build.pde
@@ -0,0 +1,5 @@
+void setup() {
+	fill(#3f3e3f);
+	rect(10, 20, 50, 50);
+	 
+}
